2155db312663cb86b2cbe4f6d7981cb794b31bad
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-11-28 17:53:46 +0200

    Fixed the issue with recipes without ingredients

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
+import { Recipe } from './../recipes/recipe.model';
 import 'rxjs/Rx';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Response } from '@angular/http/src/static_response';
@@ -16,9 +17,19 @@ export class DataStorageService {
 
   getRecipes() {
     return this.http.get('https://angular-recipe-book-43294.firebaseio.com/recipes.json')
-      .subscribe(
+      .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
           this.recipeService.setRecipes(recipes);
         }
       );
