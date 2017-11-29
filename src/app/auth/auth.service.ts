fd28580ccdd4fa1bf968887351127eda37785330
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-11-29 15:17:34 +0200

    Hid the Manage-button from the logged out users and the Register- and Login-buttons from loggedin user

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -49,4 +49,8 @@ export class AuthService {
           }
         )
     }
+
+    isAuthenticated() {
+      return this.token != null;
+    }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../auth/auth.service';
 import { Response } from '@angular/http';
 import { Component } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
@@ -8,7 +9,8 @@ import { DataStorageService } from '../shared/data-storage.service';
 })
 export class HeaderComponent {
 
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) {}
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
