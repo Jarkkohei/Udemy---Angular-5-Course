d20b82fe7486c53f80c65bf185ffca6129cb4990
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-11-30 18:08:31 +0200

    Added the auth-module and routing for it and fixed the recipe- and the shopping-list-modules.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,9 @@
-import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +11,8 @@ import { SigninComponent } from './signin/signin.component';
     SigninComponent
   ],
   imports: [
-    CommonModule,
-    SharedModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule
   ]
 })
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,3 @@
-import { NgForm } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { DropdownDirective } from './dropdown.directive';
 import { CommonModule } from '@angular/common';
@@ -6,12 +5,10 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     DropdownDirective,
-    NgForm
   ],
   exports: [
     CommonModule,
     DropdownDirective,
-    NgForm
   ]
 })
 
