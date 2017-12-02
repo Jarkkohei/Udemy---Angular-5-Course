f65907cfaf3796d907133d6334fc2ee1011e2b71
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-12-02 11:27:33 +0200

    Added the authGuards to the recipes-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canActivate: [AuthGuard] },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
