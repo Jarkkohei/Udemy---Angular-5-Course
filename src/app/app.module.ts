c507a6b27cef6642398d48b0b903fe78e3d8d2cf
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-12-03 19:07:04 +0200

    Moved the services from the app.module to the core.module and the AuthGuard to the recipes.routing.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-
-import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-
-import { AuthGuard } from './auth/auth-guard.service';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
@@ -29,7 +22,6 @@ import { CoreModule } from './core/core.module';
     AuthModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,7 +5,10 @@ import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
 
-
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @NgModule({
     declarations: [
@@ -19,6 +22,12 @@ import { AppRoutingModule } from '../app-routing.module';
     exports: [
         AppRoutingModule,
         HeaderComponent
+    ],
+    providers: [
+        ShoppingListService,
+        RecipeService,
+        DataStorageService,
+        AuthService
     ]
 })
 
